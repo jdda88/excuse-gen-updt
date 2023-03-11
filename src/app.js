b4273a268e5e09b8425fcd9e656ee595bed091d4
Juan David <jdda886@gmail.com>
2023-03-11 03:40:58 +0000

    updated exgen

--- src/app.js
+++ src/app.js
@@ -8,4 +8,23 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let wIndex = who[Math.floor(Math.random() * who.length)];
+  let aIndex = action[Math.floor(Math.random() * action.length)];
+  let waIndex = what[Math.floor(Math.random() * what.length)];
+  let weIndex = when[Math.floor(Math.random() * when.length)];
+
+  document.querySelector("#excuse").innerHTML =
+    wIndex + aIndex + waIndex + weIndex;
 };
